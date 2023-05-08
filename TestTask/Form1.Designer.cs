0a48a63d1a28e76ccbf64cd1d947ad53e8221307
MyBook <MyBook@LAPTOP-7M52D5CE>
2023-05-08 17:28:35 +0400

    Добавьте файлы проекта.

---
+++ TestTask/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TestTask
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.minWords = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minWordsNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.maxWordsNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.filesNumberNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.folderNameTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chooseFolderButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.minWordsNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWordsNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesNumberNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // minWords
+            // 
+            this.minWords.AutoSize = true;
+            this.minWords.Location = new System.Drawing.Point(78, 69);
+            this.minWords.Name = "minWords";
+            this.minWords.Size = new System.Drawing.Size(326, 16);
+            this.minWords.TabIndex = 0;
+            this.minWords.Text = "Укажите минимальное количество слов в файле";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Укажите максимальное количество слов в файле";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(434, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Укажите количество файлов, котрые необходимо сгенерировать";
+            // 
+            // minWordsNumericUpDown
+            // 
+            this.minWordsNumericUpDown.Location = new System.Drawing.Point(81, 106);
+            this.minWordsNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.minWordsNumericUpDown.Name = "minWordsNumericUpDown";
+            this.minWordsNumericUpDown.Size = new System.Drawing.Size(323, 22);
+            this.minWordsNumericUpDown.TabIndex = 3;
+            // 
+            // maxWordsNumericUpDown
+            // 
+            this.maxWordsNumericUpDown.Location = new System.Drawing.Point(81, 182);
+            this.maxWordsNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.maxWordsNumericUpDown.Name = "maxWordsNumericUpDown";
+            this.maxWordsNumericUpDown.Size = new System.Drawing.Size(323, 22);
+            this.maxWordsNumericUpDown.TabIndex = 4;
+            // 
+            // filesNumberNumericUpDown
+            // 
+            this.filesNumberNumericUpDown.Location = new System.Drawing.Point(81, 253);
+            this.filesNumberNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.filesNumberNumericUpDown.Name = "filesNumberNumericUpDown";
+            this.filesNumberNumericUpDown.Size = new System.Drawing.Size(323, 22);
+            this.filesNumberNumericUpDown.TabIndex = 5;
+            this.filesNumberNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(186, 376);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(120, 50);
+            this.acceptButton.TabIndex = 6;
+            this.acceptButton.Text = "Применить";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // folderNameTB
+            // 
+            this.folderNameTB.Location = new System.Drawing.Point(81, 322);
+            this.folderNameTB.Name = "folderNameTB";
+            this.folderNameTB.Size = new System.Drawing.Size(264, 22);
+            this.folderNameTB.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Выберите папку для генерации файлов";
+            // 
+            // chooseFolderButton
+            // 
+            this.chooseFolderButton.Location = new System.Drawing.Point(351, 307);
+            this.chooseFolderButton.Name = "chooseFolderButton";
+            this.chooseFolderButton.Size = new System.Drawing.Size(94, 43);
+            this.chooseFolderButton.TabIndex = 9;
+            this.chooseFolderButton.Text = "Выбрать";
+            this.chooseFolderButton.UseVisualStyleBackColor = true;
+            this.chooseFolderButton.Click += new System.EventHandler(this.chooseFolderButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.chooseFolderButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderNameTB);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.filesNumberNumericUpDown);
+            this.Controls.Add(this.maxWordsNumericUpDown);
+            this.Controls.Add(this.minWordsNumericUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.minWords);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Генерация файлов";
+            ((System.ComponentModel.ISupportInitialize)(this.minWordsNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxWordsNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filesNumberNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label minWords;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown minWordsNumericUpDown;
+        private System.Windows.Forms.NumericUpDown maxWordsNumericUpDown;
+        private System.Windows.Forms.NumericUpDown filesNumberNumericUpDown;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.TextBox folderNameTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button chooseFolderButton;
+    }
+}
+
---
+++ TestTask/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestTask
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string folderName;
+        private List<String> wordsForText;
+
+        private void chooseFolderButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // если папка выбрана и нажата клавиша `OK` - значит можно получить путь к папке
+            if (result == DialogResult.OK)
+            {
+                // запишем в нашу переменную путь к папке
+                folderName = folderBrowserDialog1.SelectedPath;
+                folderNameTB.Text = folderName;
+            }
+        }
+
+        //нужно создать n файлов, filesNumberNumericUpDown.text, в цикле создаем файлы с именем i.txt
+        //где i - счетчик
+        public void GenerateFiles()
+        {
+            int filesNumber = Convert.ToInt32(filesNumberNumericUpDown.Value);
+            for(int i = 0; i < filesNumber; i++)
+            {
+                string filePath = string.Format("{0}/{1}.txt", folderName, i);
+
+                using (StreamWriter fileStream = File.Exists(filePath) ? File.CreateText(filePath) : File.CreateText(filePath))
+                {
+                    
+                    fileStream.WriteLine(GenerateText());
+                }
+            }
+        }
+        //из wordsForText нужно брать рандомные слова и рандомно разделять их пробелами и знаками препинания
+        public string GenerateText()
+        {
+            var separators = new List<String>
+            {
+                " ", "," ,"." ,"!" ,"?" ,"-", ";", ":"
+            };
+            int minWords = Convert.ToInt32(minWordsNumericUpDown.Value);
+            int maxWords = Convert.ToInt32(maxWordsNumericUpDown.Value);
+            var random = new Random();
+            int wordsNumber = random.Next(minWords, maxWords);
+            string text = null;
+            for(int i = 0; i < wordsNumber; i++)
+            {
+                int wordIndex = random.Next(0,wordsForText.Count);
+                text += wordsForText[wordIndex];
+                int separatorIndex = random.Next(0, separators.Count);
+                text += separators[separatorIndex];
+            }
+
+            return text;
+        }
+
+        public List<String> ReadFile()
+        {
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;//path to exe file
+            var path = Path.Combine(exePath, "Text\\RUS.txt");
+            List<String> slova = new List<string>();
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                    slova.Add(sr.ReadLine());
+            }
+
+            return slova;
+        }
+        //нужно один раз считать файл и выбрать из него слова
+        //считывание делаем, если все поля заполнены и нет ошибок и если файл еще не считан,
+        //т.е. если переменная со словами не пустая
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            //проверить корректность заполненности полей
+            //поле пути не может быть пустым или ест=ли путь введен неверно вывести уведомление об этом
+            //генерировать должны хотя бы 1 файл
+            //минимальное количество слов не должно превышать максимальное количество слов
+            
+
+            if (string.IsNullOrEmpty(folderNameTB.Text))
+            {
+                MessageBox.Show("Укажите папку для генерации файлов!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(Convert.ToInt32(minWordsNumericUpDown.Value) > Convert.ToInt32(maxWordsNumericUpDown.Value))
+            {
+                MessageBox.Show("Минимальное количество слов не может превышать максимальное количество слов в тексте",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (Convert.ToInt32(filesNumberNumericUpDown.Value) <=0)
+            {
+                MessageBox.Show("Нужно сгенерировать хотя бы 1 файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    if (wordsForText == null)
+                    {
+                        wordsForText = ReadFile();
+                    }
+                    GenerateFiles();
+                    DialogResult result = MessageBox.Show("Файлы успешно созданы", "Успех!", MessageBoxButtons.OK);
+                }
+                catch(Exception exception)
+                {
+                    MessageBox.Show(exception.ToString(),"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+            
+        }
+    }
+}
